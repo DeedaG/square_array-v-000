11ee2dcdcc8b9af12b02a16c61c8156a0d19b41c
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-19 03:30:32 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,10 @@
 
 
 def square_array(numbers)
-  new_numbers = numbers.each do|index|
-    index ** 2
+  new_numbers = numbers.each do {|index|
+    index ** 2}
   end
   new_numbers
 end
+
+Array.new(3){ |index| index ** 2 }
